c9e44abfcb069c492d1c0d467108f9845db0674a
Luiz <fernandoluiz43673@gmail.com>
2021-07-03 20:12:29 -0300

    Conclui o painel-parceiro

--- painel-parceiro/js/pag.js
+++ painel-parceiro/js/pag.js
@@ -1,15 +1,12 @@
 function abrirAnuncios(){
     document.getElementById('anuncio').style.display = 'flex';
     document.getElementById('add-anuncio').style.display = 'none';
-    document.getElementById('rem-anuncio').style.display = 'none';
 }
 function addAnuncio(){
     document.getElementById('add-anuncio').style.display = 'flex';
     document.getElementById('anuncio').style.display = 'none';
-    document.getElementById('rem-anuncio').style.display = 'none';
 }
-function remAnuncio(){
-    document.getElementById('rem-anuncio').style.display = 'flex';
+function fecharAddAnuncio() {
     document.getElementById('add-anuncio').style.display = 'none';
-    document.getElementById('anuncio').style.display = 'none';
+    document.getElementById('anuncio').style.display = 'flex';
 }
