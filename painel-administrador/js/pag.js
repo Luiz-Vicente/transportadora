e0bab1cd9ad4b571b11f1ec8cbab36fa5d70a542
Luiz Fernando <fernandoluiz43673@gmail.com>
2021-08-08 19:33:24 -0300

    alterações

--- painel-administrador/js/pag.js
+++ painel-administrador/js/pag.js
@@ -2,23 +2,33 @@ function abrirEmbarcadora(){
     document.getElementById('embarcadora').style.display = 'flex';
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
-    document.getElementById('parceiro').style.display = 'none';
+    document.getElementById('anunciante').style.display = 'none';
 }
 function abrirTransportadora(){
     document.getElementById('transportadora').style.display = 'flex';
     document.getElementById('embarcadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
-    document.getElementById('parceiro').style.display = 'none';
+    document.getElementById('anunciante').style.display = 'none';
 }
 function abrirAutonomo(){
     document.getElementById('autonomo').style.display = 'flex';
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('embarcadora').style.display = 'none';
-    document.getElementById('parceiro').style.display = 'none';
+    document.getElementById('anunciante').style.display = 'none';
 }
-function abrirParceiro(){
-    document.getElementById('parceiro').style.display = 'flex';
+function abrirAnunciante(){
+    document.getElementById('anunciante').style.display = 'flex';
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
     document.getElementById('embarcadora').style.display = 'none';
+}
+
+
+function abrirCadEmbarcadora(){
+    document.getElementById('cad-embarcadora').style.display = 'flex';
+    document.getElementById('embarcadora').style.display = 'none';
+}
+function fecharCadEmbarcadora(){
+    document.getElementById('cad-embarcadora').style.display = 'none';
+    document.getElementById('embarcadora').style.display = 'flex';
 }
