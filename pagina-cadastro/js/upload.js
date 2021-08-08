378844f244ac86260d1e530adebb3e442d456b7a
Luiz Fernando <fernandoluiz43673@gmail.com>
2021-08-07 23:54:50 -0300

    Alterações

--- pagina-cadastro/js/upload.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-let upload = document.getElementById('upload');
-let arquivo = document.getElementById('input-upload');
-
-upload.addEventListener('click', () => {
-    arquivo.click();
-});
