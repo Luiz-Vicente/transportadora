b0f4e208b8d053b61670444465b4a83611054a92
Luiz Fernando <fernandoluiz43673@gmail.com>
2021-08-07 23:44:48 -0300

    alterações

---
+++ pagina-cadastro/autonomo/upload.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let upload = document.getElementById('upload');
+let arquivo = document.getElementById('input-upload');
+
+upload.addEventListener('click', () => {
+    arquivo.click();
+});
