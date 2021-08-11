f1ce6719d02ad550e953a109420434dbffd45baf
Luiz Fernando <fernandoluiz43673@gmail.com>
2021-08-11 00:37:44 -0300

    alterações

--- painel-administrador/js/pag.js
+++ painel-administrador/js/pag.js
@@ -3,24 +3,40 @@ function abrirEmbarcadora(){
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
     document.getElementById('anunciante').style.display = 'none';
+    document.getElementById('cad-embarcadora').style.display = 'none';
+    document.getElementById('cad-transportadora').style.display = 'none';
+    document.getElementById('cad-autonomo').style.display = 'none';
+    document.getElementById('cad-anunciante').style.display = 'none';
 }
 function abrirTransportadora(){
     document.getElementById('transportadora').style.display = 'flex';
     document.getElementById('embarcadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
     document.getElementById('anunciante').style.display = 'none';
+    document.getElementById('cad-embarcadora').style.display = 'none';
+    document.getElementById('cad-transportadora').style.display = 'none';
+    document.getElementById('cad-autonomo').style.display = 'none';
+    document.getElementById('cad-anunciante').style.display = 'none';
 }
 function abrirAutonomo(){
     document.getElementById('autonomo').style.display = 'flex';
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('embarcadora').style.display = 'none';
     document.getElementById('anunciante').style.display = 'none';
+    document.getElementById('cad-embarcadora').style.display = 'none';
+    document.getElementById('cad-transportadora').style.display = 'none';
+    document.getElementById('cad-autonomo').style.display = 'none';
+    document.getElementById('cad-anunciante').style.display = 'none';
 }
 function abrirAnunciante(){
     document.getElementById('anunciante').style.display = 'flex';
     document.getElementById('transportadora').style.display = 'none';
     document.getElementById('autonomo').style.display = 'none';
     document.getElementById('embarcadora').style.display = 'none';
+    document.getElementById('cad-embarcadora').style.display = 'none';
+    document.getElementById('cad-transportadora').style.display = 'none';
+    document.getElementById('cad-autonomo').style.display = 'none';
+    document.getElementById('cad-anunciante').style.display = 'none';
 }
 
 
@@ -31,4 +47,31 @@ function abrirCadEmbarcadora(){
 function fecharCadEmbarcadora(){
     document.getElementById('cad-embarcadora').style.display = 'none';
     document.getElementById('embarcadora').style.display = 'flex';
+}
+
+function abrirCadTransportadora(){
+    document.getElementById('cad-transportadora').style.display = 'flex';
+    document.getElementById('transportadora').style.display = 'none';
+}
+function fecharCadTransportadora(){
+    document.getElementById('cad-transportadora').style.display = 'none';
+    document.getElementById('transportadora').style.display = 'flex';
+}
+
+function abrirCadAutonomo(){
+    document.getElementById('cad-autonomo').style.display = 'flex';
+    document.getElementById('autonomo').style.display = 'none';
+}
+function fecharCadAutonomo(){
+    document.getElementById('cad-autonomo').style.display = 'none';
+    document.getElementById('autonomo').style.display = 'flex';
+}
+
+function abrirCadAnunciante(){
+    document.getElementById('cad-anunciante').style.display = 'flex';
+    document.getElementById('anunciante').style.display = 'none';
+}
+function fecharCadAnunciante(){
+    document.getElementById('cad-anunciante').style.display = 'none';
+    document.getElementById('anunciante').style.display = 'flex';
 }
--- painel-embarcadora/js/pag.js
+++ painel-embarcadora/js/pag.js
@@ -2,14 +2,30 @@ function abrirContratar(){
     document.getElementById('contratar').style.display = 'flex';
     document.getElementById('viagem').style.display = 'none';
     document.getElementById('sinistro').style.display = 'none';
+    document.getElementById('transportadora-contratada').style.display = 'none';
 }
 function abrirViagem(){
     document.getElementById('viagem').style.display = 'flex';
     document.getElementById('contratar').style.display = 'none';
     document.getElementById('sinistro').style.display = 'none';
+    document.getElementById('transportadora-contratada').style.display = 'none';
 }
 function abrirSinistro(){
     document.getElementById('sinistro').style.display = 'flex';
     document.getElementById('viagem').style.display = 'none';
     document.getElementById('contratar').style.display = 'none';
+    document.getElementById('transportadora-contratada').style.display = 'none';
+}
+
+function abrirPedido(){
+    document.getElementById('transportadora-contratada').style.display = 'flex';
+    document.getElementById('viagem').style.display = 'none';
+    document.getElementById('contratar').style.display = 'none';
+    document.getElementById('sinistro').style.display = 'none';
+}
+function fecharPedido(){
+    document.getElementById('transportadora-contratada').style.display = 'none';
+    document.getElementById('viagem').style.display = 'flex';
+    document.getElementById('contratar').style.display = 'none';
+    document.getElementById('sinistro').style.display = 'none';
 }
