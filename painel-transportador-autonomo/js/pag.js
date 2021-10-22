6b7e5bb9654a458627bb8a3871fcf9a2fea14b91
Luiz Fernando <fernandoluiz43673@gmail.com>
2021-10-22 14:46:01 -0300

    transp-autonomo

--- painel-transportador-autonomo/js/pag.js
+++ painel-transportador-autonomo/js/pag.js
@@ -1,53 +1,24 @@
-function abrirCaminhoes(){
-    document.getElementById('caminhoes').style.display = 'flex';
-    document.getElementById('viagens').style.display = 'none';
-    document.getElementById('sinistros').style.display = 'none';
-    document.getElementById('responde-viagem').style.display = 'none';
-    document.getElementById('add-sinistro').style.display = 'none';
-    document.getElementById('responde-viagem').style.display = 'none';
-}
 function abrirViagens(){
     document.getElementById('viagens').style.display = 'flex';
-    document.getElementById('caminhoes').style.display = 'none';
-    document.getElementById('sinistros').style.display = 'none';
-    document.getElementById('add-caminhao').style.display = 'none';
-    document.getElementById('add-sinistro').style.display = 'none';
-    document.getElementById('responde-viagem').style.display = 'none';
+    document.getElementById('dados-ajudante').style.display = 'none';
+    document.getElementById('dados-caminhao').style.display = 'none';
+    document.getElementById('dados-transp-autonomo').style.display = 'none';
 }
-function abrirSinistros(){
-    document.getElementById('sinistros').style.display = 'flex';
-    document.getElementById('caminhoes').style.display = 'none';
+function abrirDadosCaminhao(){
     document.getElementById('viagens').style.display = 'none';
-    document.getElementById('add-caminhao').style.display = 'none';
-    document.getElementById('responde-viagem').style.display = 'none';
-    document.getElementById('responde-viagem').style.display = 'none';
+    document.getElementById('dados-ajudante').style.display = 'none';
+    document.getElementById('dados-caminhao').style.display = 'flex';
+    document.getElementById('dados-transp-autonomo').style.display = 'none';
 }
-
-
-
-function abrirAddcaminhao(){
-    document.getElementById('add-caminhao').style.display = 'flex';
-    document.getElementById('caminhoes').style.display = 'none';
-}
-function fecharAddcaminhao(){
-    document.getElementById('add-caminhao').style.display = 'none';
-    document.getElementById('caminhoes').style.display = 'flex';
-}
-
-function abrirRespondeviagem(){
-    document.getElementById('responde-viagem').style.display = 'flex';
+function abrirDadosAjudante(){
     document.getElementById('viagens').style.display = 'none';
+    document.getElementById('dados-ajudante').style.display = 'flex';
+    document.getElementById('dados-caminhao').style.display = 'none';
+    document.getElementById('dados-transp-autonomo').style.display = 'none';
 }
-function fecharRespondeviagem(){
-    document.getElementById('responde-viagem').style.display = 'none';
-    document.getElementById('viagens').style.display = 'flex';
-}
-
-function abrirAddsinistro(){
-    document.getElementById('add-sinistro').style.display = 'flex';
-    document.getElementById('sinistros').style.display = 'none';
-}
-function fecharAddsinistro(){
-    document.getElementById('add-sinistro').style.display = 'none';
-    document.getElementById('sinistros').style.display = 'flex';
+function abrirMeusDados(){
+    document.getElementById('viagens').style.display = 'none';
+    document.getElementById('dados-ajudante').style.display = 'none';
+    document.getElementById('dados-caminhao').style.display = 'none';
+    document.getElementById('dados-transp-autonomo').style.display = 'flex';
 }
